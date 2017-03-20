39a0121c7b3f1353d0ea0b249497183fd0bee0dc
shaalini Ajay <shaaliniajay@gmail.com>
2017-03-20 11:05:06 +1100

    Added data model

---
+++ NorthWindWebAPI/NorthWindWebAPI/DataModel/Northwind.cs
@@ -0,0 +1,24 @@
+namespace NorthWindWebAPI.DataModel
+{
+    using System;
+    using System.Data.Entity;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Linq;
+
+    public partial class Northwind : DbContext
+    {
+        public Northwind()
+            : base("name=Northwind")
+        {
+        }
+
+        public virtual DbSet<Customer> Customers { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Customer>()
+                .Property(e => e.CustomerID)
+                .IsFixedLength();
+        }
+    }
+}
